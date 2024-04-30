7fac597bdd054dad8a3e3e958183e499454ed5ac
itsjackie00 <jackiedanso33@gmail.com>
2024-04-30 17:41:07 +0200

    added snack 3

--- index.php
+++ index.php
@@ -78,6 +78,52 @@ Con un form passare come parametri GET name, mail e age e verificare
  “Accesso riuscito”, altrimenti “Accesso negato”
 */
 ?>
+<?php
+//SNACK 3
+/*
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: 
+    DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+    Stampare ogni data con i relativi post.
+*/
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -118,6 +164,18 @@ Con un form passare come parametri GET name, mail e age e verificare
             }
         };
     ?>
+    </section>
+    <section>
+        <h2>Snack 3</h2>
+        <?php
+        foreach($posts as $key => $value) {
+            echo ($key) . "<br>";
+            echo $value[0]['title'] . "<br>";
+            echo $value[0]['author'] . "<br>";
+            echo $value[0]['text'] . "<br><br>";
+            //echo print_r($value);
+        }
+        ?>
     </section>
 </body>
 
