887a7d29340d288dd89c11047ce24fe6d5295e61
itsjackie00 <jackiedanso33@gmail.com>
2024-04-30 17:21:02 +0200

    added snack 2

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-// Snack 1
+//SNACK 1
 // Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 // Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa 
 //e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
 // Olimpia Milano - Cantù | 55-60
-
-
 $matches = [
     [
         "casa" => "Varese",
@@ -70,6 +68,16 @@ $matches = [
 ];
 var_dump($matches);
 ?>
+<?php
+//SNACK 2
+/* 
+Snack 2
+Con un form passare come parametri GET name, mail e age e verificare 
+(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
+ che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare 
+ “Accesso riuscito”, altrimenti “Accesso negato”
+*/
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -81,12 +89,36 @@ var_dump($matches);
 </head>
 
 <body>
+    <section>
     <h1>Snack-1</h1>
     <?php
     foreach ($matches as $match) {
         echo $match["casa"] . " - " . $match["ospite"] . " | " . $match["casaPunti"] . " - " . $match["ospitePunti"] . "<br>";   
     }
     ?>
+    </section>
+    <section>
+    <h2>Snack-2</h2>
+    <form action="index.php" method="GET">
+        <label for="name">Insert name</label>
+        <input type="text" name="name" id="name">
+        <label for="age">Insert age</label>
+        <input type="text" name="age" id="age">
+        <label for="mail">Insert mail</label>
+        <input type="text" name="mail" id="mail">
+        <button type="submit">Send</button>
+
+    </form>
+    <?php 
+        if(isset($_GET["name"]) && isset($_GET["age"]) && isset($_GET["mail"])) {
+            if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], ".") && strpos($_GET["mail"], "@") && is_numeric($_GET["age"])) {
+                echo "Accesso riuscito";
+            } else {
+                echo "Accesso negato";
+            }
+        };
+    ?>
+    </section>
 </body>
 
 </html>
