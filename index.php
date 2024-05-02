b75632e53df765463fd8bb4916c9eeef240abfa2
itsjackie00 <jackiedanso33@gmail.com>
2024-05-02 09:57:41 +0200

    added navbar

--- index.php
+++ index.php
@@ -139,44 +139,68 @@ $numbers = array();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Snack</title>
 </head>
 
 <body>
+    <header class="container d-flex justify-content-between">
+        <h1>PHP Snacks</h1>
+        <nav>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="snack-1.php">Snack-1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="snack-2.php">Snack-2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="snack-3.php">Snack-3</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="snack-4.php">Snack-4</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
     <section>
-    <h1>Snack-1</h1>
-    <?php
-    foreach ($matches as $match) {
-        echo $match["casa"] . " - " . $match["ospite"] . " | " . $match["casaPunti"] . " - " . $match["ospitePunti"] . "<br>";   
-    }
-    ?>
+        <h1>Snack-1</h1>
+        <?php
+        foreach ($matches as $match) {
+            echo $match["casa"] . " - " . $match["ospite"] . " | " . $match["casaPunti"] . " - " . $match["ospitePunti"] . "<br>";
+        }
+        ?>
     </section>
     <section>
-    <h2>Snack-2</h2>
-    <form action="index.php" method="GET">
-        <label for="name">Insert name</label>
-        <input type="text" name="name" id="name">
-        <label for="age">Insert age</label>
-        <input type="text" name="age" id="age">
-        <label for="mail">Insert mail</label>
-        <input type="text" name="mail" id="mail">
-        <button type="submit">Send</button>
+        <h2>Snack-2</h2>
+        <form action="index.php" method="GET">
+            <label for="name">Insert name</label>
+            <input type="text" name="name" id="name">
+            <label for="age">Insert age</label>
+            <input type="text" name="age" id="age">
+            <label for="mail">Insert mail</label>
+            <input type="text" name="mail" id="mail">
+            <button type="submit">Send</button>
 
-    </form>
-    <?php 
-        if(isset($_GET["name"]) && isset($_GET["age"]) && isset($_GET["mail"])) {
-            if(strlen($_GET["name"]) > 3 && strpos($_GET["mail"], ".") && strpos($_GET["mail"], "@") && is_numeric($_GET["age"])) {
+        </form>
+        <?php
+        if (isset($_GET["name"]) && isset($_GET["age"]) && isset($_GET["mail"])) {
+            if (strlen($_GET["name"]) > 3 && strpos($_GET["mail"], ".") && strpos($_GET["mail"], "@") && is_numeric($_GET["age"])) {
                 echo "Accesso riuscito";
             } else {
                 echo "Accesso negato";
             }
         };
-    ?>
+        ?>
     </section>
     <section>
         <h2>Snack 3</h2>
         <?php
-        foreach($posts as $key => $value) {
+        foreach ($posts as $key => $value) {
             echo ($key) . "<br>";
             echo $value[0]['title'] . "<br>";
             echo $value[0]['author'] . "<br>";
@@ -188,9 +212,9 @@ $numbers = array();
     <section>
         <h2>Snack 4</h2>
         <?php
-        for($i = 0; $i < 15; $i++) {
+        for ($i = 0; $i < 15; $i++) {
             $number = rand(1, 100);
-            if(!in_array($number, $numbers)) {
+            if (!in_array($number, $numbers)) {
                 $numbers[] = $number;
                 echo $number . "<br>";
             }
